8edd1f5b8bb778075d0e21cbf4230557c5a3d4dc
verlil <vernigoralilia@gmail.com>
2018-09-29 16:13:35 +0300

    imported AngularMaterial, created basic component structure

---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule]
+})
+
+export class MaterialModule {
+}
