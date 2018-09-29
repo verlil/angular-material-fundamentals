805a65d27823908ec2c278b29ca33e6a400f612f
verlil <vernigoralilia@gmail.com>
2018-09-29 16:53:33 +0300

    flex introduction

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,27 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule]
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ]
 })
 
 export class MaterialModule {
